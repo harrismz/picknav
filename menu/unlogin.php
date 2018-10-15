360540911abef0743a71fbc89d265c14bdca734d
Harris Muhammad <harris.git21@gmail.com>
2018-10-15 20:36:35 +0700

    Old Picknav Part 7 ( add folder unlogin )

---
+++ menu/unlogin.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+	<html lang="en" class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7">
+		
+		<!--head-->
+		<head>
+			<title>PICKING NAVIGATION SYSTEM</title>
+			<link rel="shortcut icon" href="asset/icon/pi.png"/>
+
+			<!--charset db-->
+			<meta charset="utf-8"/>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+			<meta name="google" value="notranslate"/>
+			
+			<!--refresh when session is loss 
+			<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">-->S
+   
+			<!--bootstrap-->
+			<meta name="viewport"  content="width=device-width, initial-scale=1"> <!-- for detection width resolution -->
+			<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+
+			<!--css-->
+			<link rel="stylesheet" type="text/css" href="asset/css/style.css">
+			
+		</head>
+		<!--====-->
+		
+		<!--body-->
+		<body>
+			<nav class="navbar navbar-default navbar-fixed-top">
+				<?php //include "navigation.php"; ?>
+			</nav>
+			<section style="text-align:center;">
+				<br><br><br>
+				<h1>You are not logged in.</h1>
+				<br>
+				<h4><a href="index.php"> click here for LOGIN </a></h4>
+			</section>
+		</body>
+		<!--====-->
+		
+		<!--script-->
+		<script  type="text/javascript" src="../bootstrap/jquery/jquery-1.12.0.min.js"></script>
+		<script  type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+		<!--====-->
+		
+	</html>
+	<!--====-->
